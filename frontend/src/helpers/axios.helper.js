961cdb8bdb345e7f387c33087c3535262b442d40
Bhumika-0204 <bhumikakumari0802@gmail.com>
2026-04-08 02:03:36 +0530

    chore: setup VITE_BACKEND_URL for production deployment

--- frontend/src/helpers/axios.helper.js
+++ frontend/src/helpers/axios.helper.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const baseURL = "http://localhost:8000/api/v1";
+const baseURL = import.meta.env.VITE_BACKEND_URL || "http://localhost:8000/api/v1";
 
 export const axiosInstance = axios.create({
   baseURL,
